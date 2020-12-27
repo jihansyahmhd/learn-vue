ceb5e3f783779083caa4fb8ee72f80554dd00faf
jihansyahmhd <jihansyahmuhamad1@gmail.com>
2020-12-28 01:36:47 +0700

    tutorial 23

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <div >
-    <h1>{{title}}</h1>
-    <ninjas></ninjas>
+  <div>
+      <app-header></app-header>
+      <app-ninjas v-bind:ninjas='ninjadata'></app-ninjas>
+      <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import Ninjas from './Ninjas.vue'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
+
 export default {
   components:{
-    'ninjas':Ninjas
+    'app-header':Header,
+    'app-footer':Footer,
+    'app-ninjas':Ninjas
   },
   data () {
     return {
-      title:'Ninja App'
+              ninjadata: [
+                    {name: 'Ryu', speciality: 'Vue Components', show: false},
+                    {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+                    {name: 'Hitoshi', speciality: 'Click Events', show: false},
+                    {name: 'Tango', speciality: 'Conditionals', show: false},
+                    {name: 'Kami', speciality: 'Webpack', show: false},
+                    {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+                ]
     }
   }
 }
 </script>
 
 <style scoped>
-  h1{
-    color:green
-  }
+
 </style>
